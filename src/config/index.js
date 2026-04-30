4b1e5ab3bf9476d5cc80e54b9ff61f6c439fb298
AymanFarra2004 <aymanelfarra2004@gmail.com>
2026-04-30 16:01:25 +0300

    fix: fix the typescript compiling issue at CONFIG obj

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,11 @@
+if (!process.env.NEXT_PUBLIC_API_URL) {
+    throw new Error("NEXT_PUBLIC_API_URL is not defined in environment variables");
+} else {
+  const APIURL = process.env.NEXT_PUBLIC_API_URL;
+}
+
 export const CONFIG = {
-  API_URL: process.env.NEXT_PUBLIC_API_URL ,
+  API_URL: APIURL ,
   APP_URL: process.env.NEXT_PUBLIC_APP_URL || "https://www.qareeb.cc",
   SUPPORT_EMAIL: process.env.NEXT_PUBLIC_SUPPORT_EMAIL || "support@qareeb.cc",
 };
